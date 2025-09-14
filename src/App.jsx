879ae291b85ac71ecf8b51568b9e8bf72ffb6300
Mohammad Ashfaqul Hasan <166525758+ashfaqOPU@users.noreply.github.com>
2025-09-14 19:25:25 +0600

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 	return (
 		<>
 			<StoryblokComponent blok={story.content} />
-			<footer>All rights reservedss © {currentYear}</footer>
+			<footer>All risaaghts reservedss © {currentYear}</footer>
 		</>
 	);
 }
