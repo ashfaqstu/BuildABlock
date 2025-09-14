7998e91bdea10039e37ef7f098bdf691ec9ee416
Mohammad Ashfaqul Hasan <2305102@ugrad.cse.buet.ac.bd>
2025-09-14 22:06:03 +0600

    new

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 	return (
 		<>
 			<StoryblokComponent blok={story.content} />
-			<footer>All arisadgfgaghts reservedss © {currentYear}</footer>
+			<footer>Build with happiness © {currentYear}</footer>
 		</>
 	);
 }
